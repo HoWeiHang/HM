9a7de780c11394532c2d192e1fda9170990368bf
HoWeiHang <wkite2001@gmail.com>
2016-08-28 11:34:38 +0800

    add setRingtone

--- app/src/main/java/fju/im2016/com/hm/ui/component/AdapterMenuSelectedListener.java
+++ app/src/main/java/fju/im2016/com/hm/ui/component/AdapterMenuSelectedListener.java
@@ -1,21 +1,30 @@
 package fju.im2016.com.hm.ui.component;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.view.menu.MenuBuilder;
 import android.support.v7.widget.PopupMenu;
 import android.view.MenuItem;
 import android.view.View;
+
+import java.io.File;
 
 import fju.im2016.com.hm.R;
 import fju.im2016.com.hm.core.entity.Song;
 
 public class AdapterMenuSelectedListener implements View.OnClickListener {
     private Context context;
-    private Song Song;
+    private Song song;
+    private String newAddress, newFilename;
+    private int countAddress;
+    private char[] c;
 
     public AdapterMenuSelectedListener(Context context, Song song) {
         this.context = context;
-        this.Song = song;
+        this.song = song;
     }
 
     @Override
@@ -26,6 +35,13 @@ public class AdapterMenuSelectedListener implements View.OnClickListener {
             public boolean onMenuItemSelected(MenuBuilder menu, MenuItem item) {
                 switch (item.getItemId()) {
                     case R.id.adapter_menu_setRingTone:
+                        newAddress = "";
+                        newFilename = "";
+                        c = String.valueOf(song.getPath()).toCharArray();
+                        getCountAddress();
+                        getNewAddress();
+                        getNewFilename();
+                        setRingtones();
                         return true;
 
                     case R.id.adapter_menu_delete:
@@ -43,5 +59,48 @@ public class AdapterMenuSelectedListener implements View.OnClickListener {
         popupMenu.inflate(R.menu.menu_adapter);
 
         popupMenu.show();
+    }
+
+    private int getCountAddress() {
+        for (int i = 0; i < this.c.length; i++) {
+            if (this.c[i] == '/') {
+                this.countAddress = i;
+            }
+        }
+        return this.countAddress;
+    }
+
+    private String getNewAddress() {
+        for (int i = 0; i <= this.countAddress; i++) {
+            this.newAddress += this.c[i];
+        }
+        return this.newAddress;
+    }
+
+    private String getNewFilename() {
+        for (int i = this.countAddress + 1; i < this.c.length; i ++){
+            this.newFilename += this.c[i];
+        }
+        return this.newFilename;
+    }
+
+    private void setRingtones() {
+        File file = new File(this.newAddress, this.newFilename);
+
+        ContentValues values = new ContentValues();
+        values.put(MediaStore.MediaColumns.DATA, file.getAbsolutePath());
+        values.put(MediaStore.MediaColumns.TITLE, this.newFilename);
+        values.put(MediaStore.MediaColumns.MIME_TYPE, "audio/*");
+
+        values.put(MediaStore.Audio.Media.IS_RINGTONE, true);
+        values.put(MediaStore.Audio.Media.IS_NOTIFICATION, false);
+        values.put(MediaStore.Audio.Media.IS_ALARM, false);
+        values.put(MediaStore.Audio.Media.IS_MUSIC, false);
+
+        Uri uri = MediaStore.Audio.Media.getContentUriForPath(file.getAbsolutePath());
+        this.context.getContentResolver().delete(uri, MediaStore.MediaColumns.DATA + "=\"" + file.getAbsolutePath() + "\"", null);
+        Uri newUri = this.context.getApplicationContext().getContentResolver().insert(uri, values);
+
+        RingtoneManager.setActualDefaultRingtoneUri(this.context.getApplicationContext(), RingtoneManager.TYPE_RINGTONE, newUri);
     }
 }
