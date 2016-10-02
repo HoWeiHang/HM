76b10f55be55a6e4b295531596be6f53c4e45d68
HoWeiHang <wkite2001@gmail.com>
2016-10-02 20:43:59 +0800

    add shake function

--- app/src/main/java/fju/im2016/com/hm/ui/main/MainActivity.java
+++ app/src/main/java/fju/im2016/com/hm/ui/main/MainActivity.java
@@ -11,6 +11,10 @@ import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.graphics.drawable.ColorDrawable;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.media.RingtoneManager;
 import android.net.Uri;
 import android.provider.MediaStore;
@@ -153,6 +157,20 @@ public class MainActivity extends AppCompatActivity implements PlayerView, ListV
     SlidingLayoutManager c;
 
     private int duration;
+
+    private SensorManager mSensorManager;   //體感(Sensor)使用管理
+    private Sensor mSensor;                 //體感(Sensor)類別
+    private float mLastX;                    //x軸體感(Sensor)偏移
+    private float mLastY;                    //y軸體感(Sensor)偏移
+    private float mLastZ;                    //z軸體感(Sensor)偏移
+    private double mSpeed;                 //甩動力道數度
+    private long mLastUpdateTime;           //觸發時間
+
+    //甩動力道數度設定值 (數值越大需甩動越大力，數值越小輕輕甩動即會觸發)
+    private static final int SPEED_SHRESHOLD = 3000;
+
+    //觸發間隔時間
+    private static final int UPTATE_INTERVAL_TIME = 70;
 
     @Override
     public void onClick(SongManager songManager) {
@@ -438,7 +456,67 @@ public class MainActivity extends AppCompatActivity implements PlayerView, ListV
         });
 
 
+        this.iniSensor();
     }
+
+    private void iniSensor() {
+        //取得體感(Sensor)服務使用權限
+        mSensorManager = (SensorManager) this.getSystemService(Context.SENSOR_SERVICE);
+
+        //取得手機Sensor狀態設定
+        mSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
+        //註冊體感(Sensor)甩動觸發Listener
+        mSensorManager.registerListener(SensorListener, mSensor, SensorManager.SENSOR_DELAY_GAME);
+    }
+
+    private SensorEventListener SensorListener = new SensorEventListener()
+    {
+        public void onSensorChanged(SensorEvent mSensorEvent)
+        {
+            //當前觸發時間
+            long mCurrentUpdateTime = System.currentTimeMillis();
+
+            //觸發間隔時間 = 當前觸發時間 - 上次觸發時間
+            long mTimeInterval = mCurrentUpdateTime - mLastUpdateTime;
+
+            //若觸發間隔時間< 70 則return;
+            if (mTimeInterval < UPTATE_INTERVAL_TIME) return;
+
+            mLastUpdateTime = mCurrentUpdateTime;
+
+            //取得xyz體感(Sensor)偏移
+            float x = mSensorEvent.values[0];
+            float y = mSensorEvent.values[1];
+            float z = mSensorEvent.values[2];
+
+            //甩動偏移速度 = xyz體感(Sensor)偏移 - 上次xyz體感(Sensor)偏移
+            float mDeltaX = x - mLastX;
+            float mDeltaY = y - mLastY;
+            float mDeltaZ = z - mLastZ;
+
+            mLastX = x;
+            mLastY = y;
+            mLastZ = z;
+
+            //體感(Sensor)甩動力道速度公式
+            mSpeed = Math.sqrt(mDeltaX * mDeltaX + mDeltaY * mDeltaY + mDeltaZ * mDeltaZ)/ mTimeInterval * 10000;
+
+            //若體感(Sensor)甩動速度大於等於甩動設定值則進入 (達到甩動力道及速度)
+            if (mSpeed >= SPEED_SHRESHOLD)
+            {
+                //達到搖一搖甩動後要做的事情
+                Log.d("TAG", "搖一搖中...");
+                playerPresenter.clickPlay();
+                updateBtnPlayImage();
+                updatePanelPlayImage();
+            }
+        }
+
+        public void onAccuracyChanged(Sensor sensor , int accuracy)
+        {
+        }
+    };
 
     private void setupDrawerContent(NavigationView navigationView) {
         navigationView.setNavigationItemSelectedListener(
@@ -1572,6 +1650,7 @@ public class MainActivity extends AppCompatActivity implements PlayerView, ListV
     protected void onResume() {
         super.onResume();
         getSupportActionBar().setTitle("" + nowTitle);
+        mSensorManager.registerListener(SensorListener, mSensor, SensorManager.SENSOR_DELAY_GAME);
     }
 
     @Override
@@ -1582,6 +1661,14 @@ public class MainActivity extends AppCompatActivity implements PlayerView, ListV
             return true;
         }
         return super.onKeyDown(keyCode, event);
+    }
+
+    @Override
+    protected void onDestroy()
+    {
+        super.onDestroy();
+        //在程式關閉時移除體感(Sensor)觸發
+        mSensorManager.unregisterListener(SensorListener);
     }
 
     //    @Override
